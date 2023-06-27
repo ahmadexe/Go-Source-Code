aba0041579038c189e61b691fe3f6a0b989bc8b1
ahmadexe <muahmad710@gmail.com>
2023-06-27 18:01:21 +0500

    code added for mod -mux is used

--- backend_basics/mod_stuff/main.go
+++ backend_basics/mod_stuff/main.go
@@ -1,8 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	hello()
 }
 
+func hello() {
+	fmt.Println("Hello!")
+	r := mux.NewRouter()
+	r.HandleFunc("/", serveHome).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	// Request deals with the parameters of the request, URL, headers, body, etc.
+	// ResponseWriter is used to fill in the HTTP response. Here our simple response is just “hello\n”.
+	w.Write([]byte("<h1>Hello World</h1>"))
+}
